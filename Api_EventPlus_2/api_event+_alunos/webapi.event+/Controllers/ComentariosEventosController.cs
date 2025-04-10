4cc2765956dc6873ba96271779b1b19564d4a147
RIKELME MIRANDA SILVA <47540013893@info.local>
2025-04-10 08:33:12 -0300

    Feat: Subindo projeto com implementacao de IA(Inteligencia artificial)

---
+++ Api_EventPlus_2/api_event+_alunos/webapi.event+/Controllers/ComentariosEventosController.cs
@@ -0,0 +1,122 @@
+﻿using Azure;
+using Azure.AI.ContentSafety;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapi.event_.Domains;
+using webapi.event_.Interfaces;
+
+namespace webapi.event_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ComentariosEventosController : ControllerBase
+    {
+        private readonly IComentariosEventosRepository _comentariosEventosRepository;
+
+        private readonly ContentSafetyClient _contentSafetyClient;
+
+        public ComentariosEventosController(ContentSafetyClient contentSafetyClient, IComentariosEventosRepository comentariosEventosRepository)
+        {
+
+            _comentariosEventosRepository = comentariosEventosRepository;
+            _contentSafetyClient = contentSafetyClient;
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(ComentariosEventos comentario)
+        {
+            try
+            {
+
+                if (string.IsNullOrEmpty(comentario.Descricao))
+                {
+                    return BadRequest("Descrição do comentário não pode ser vazia!");
+
+                }
+                //Criar objeto de analise do content safety 
+                var request = new AnalyzeTextOptions(comentario.Descricao);
+
+                //chamar a api do content safety
+                Response<AnalyzeTextResult> response = await _contentSafetyClient.AnalyzeTextAsync(request);
+
+
+                //Verificar se o texto anaalisado tem alguma sveridade(Termo ofensivo) 
+                bool temConteudoImroprio = response.Value.CategoriesAnalysis.Any(c => c.Severity > 0);
+
+                // Se o texto tiver conteudo impróprio, não exibe o comentario
+                comentario.Exibe = !temConteudoImroprio;
+
+                //Cadastra de fato o comentario 
+                _comentariosEventosRepository.Cadastrar(comentario);
+
+                return Ok();
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            try
+            {
+                _comentariosEventosRepository.Deletar(id);
+
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("ListarSomenteExibe")]
+        public IActionResult GetExibe(Guid id)
+        {
+            try
+            {
+                return Ok(_comentariosEventosRepository.ListarSomenteExibe(id));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Get(Guid id)
+        {
+            try
+            {
+                return Ok(_comentariosEventosRepository.Listar(id));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpGet("BuscarPorIdUsuario")]
+        public IActionResult GetByIdUser(Guid idUsuario, Guid idEvento)
+        {
+            try
+            {
+                return Ok(_comentariosEventosRepository.BuscarPorIdUsuario(idUsuario, idEvento));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
+
+    }
+}
